b43afbc9c243eb72cc048f4471d7c5170c1de222
sujay dey <sujay.dey@mountblue.tech>
2020-06-16 15:20:27 +0530

    add middleware in the project for autherization

--- modals/validate.js
+++ modals/validate.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 function validateUser(user) {
   const Schema = {
     username: Joi.string().min(2).max(50).required(),
-    phone: Joi.number().min(10).max(11).required(),
+    phone: Joi.number().min(10).required(),
     password: Joi.string().min(5).max(255).required(),
   };
 
@@ -12,7 +12,7 @@ function validateUser(user) {
 
 function validateUserSign(user) {
     const Schema = {
-      phone: Joi.number().min(10).max(11).required(),
+      phone: Joi.number().min(10).required(),
       password: Joi.string().min(5).max(255).required(),
     };
   
--- routes/api.js
+++ routes/api.js
@@ -3,15 +3,16 @@ const router = require('express').Router();
 const _ = require('lodash');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const auth = require('../middleware/auth');
 const { Users, Chat } = require('../modals/modals');
-const {client}=require('../server')
+const { client } = require('../server');
 const { jwtkey } = require('../config/key');
-const { validateUser,validateUserSign} = require('../modals/validate');
+const { validateUser, validateUserSign } = require('../modals/validate');
 
 client.on('connection', (socket) => {
   console.log('client is connected');
-  socket.on('chats', async (data) => {    
-    console.log('hello',data)
+  socket.on('chats', async (data) => {
+    console.log('hello', data);
     const users = await Users.findOne({ _id: data.id });
     if (!users) return res.send("user doesn't exsit");
     const user = new Chat(
@@ -19,81 +20,85 @@ client.on('connection', (socket) => {
     );
     users.chats.push(user);
     await users.save();
-    client.emit('userchat', users.chats);       
-    });
-  })
-  
-    router.post('/signin',async (req, res) => {
-      try {
-        const { error } = validateUserSign(req.body);
-        if (error) return res.json({success:false,username:'Invalid user input'});
-        const user = await Users.findOne({ phone: req.body.phone });
-        if (!user) return res.json({success:false,username:"User Doesn't Exist"});
-      const validpassword = bcrypt.compare(req.body.password, user.password);
-      if (!validpassword) return res.json({success:false,username:"User password is incorrect"});
-      const token = jwt.sign({ _id: user._id }, jwtkey);
-      user.token=token;
-      res.json(_.pick(user, ['_id', 'username', 'phone','token']))
+    client.emit('userchat', users.chats);
+  });
+});
 
-      } catch (error) {
-        console.log(error)
-        res.status(500).send('something failed');
-      }
-    });
-
-    router.post('/registration',async (req, res) => {
-      try {
-        const { error } = validateUser(req.body);
-        if (error) return res.json({success:true,username:'Invalid user input'});
-        const user = await Users.findOne({ username:req.body.username,phone: req.body.phone });
-        if (user)
-          return res.json({success:true,username:user.username+' already register Please Signin'});
-        const login = new Users(
-          _.pick(req.body, ['username','phone','password'])
-        );
-        const salt = await bcrypt.genSalt(5);
-        const hashed = await bcrypt.hash(login.password, salt);
-        login.password = hashed;
-        const users = await login.save();
-        return res.json({success:true,username:users.username+' successfully register please Signin'});
-      } catch (error) {
-        console.log(error)
-        res.status(500).send('something failed');
-      }
-    });
-
-
-
-
-  
-    router.get('/chat/:id', async (req, res) => {
-      try {
-        console.log('hello')
-        const user = await Users.findOne({ _id: req.params.id });
-        return res.json(user.chats);
-      } catch (errror) {
-        res.status(500).send('something failed');
-      }
-    });
-  
-    
-    router.get('/users', () => {
-      Users.find()
-        .then((obj) => {
-          sendsUserData(obj);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-   
-        
+router.post('/signin', async (req, res) => {
+  try {
+    const { error } = validateUserSign(req.body);
+    if (error)
+      return res.json({ success: false, username: 'Invalid user input' });
+    const user = await Users.findOne({ phone: req.body.phone });
+    if (!user)
+      return res.json({ success: false, username: "User Doesn't Exist" });
+    const validpassword = bcrypt.compare(req.body.password, user.password);
+    if (!validpassword)
+      return res.json({
+        success: false,
+        username: 'User password is incorrect',
       });
-      sendsUserData=(obj)=>{
-                client.emit('userdata', obj);
-              }
-      
+    const token = jwt.sign({ _id: user._id }, jwtkey);
+    user.token = token;
+    res.json(_.pick(user, ['_id', 'username', 'phone', 'token']));
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('something failed');
+  }
+});
 
+router.post('/registration', async (req, res) => {
+  try {
+    const { error } = validateUser(req.body);
+    if (error)
+      return res.json({ success: true, username: 'Invalid user input' });
+    const user = await Users.findOne({
+      username: req.body.username,
+      phone: req.body.phone,
+    });
+    if (user)
+      return res.json({
+        success: true,
+        username: user.username + ' already register Please Signin',
+      });
+    const login = new Users(
+      _.pick(req.body, ['username', 'phone', 'password'])
+    );
+    const salt = await bcrypt.genSalt(5);
+    const hashed = await bcrypt.hash(login.password, salt);
+    login.password = hashed;
+    const users = await login.save();
+    return res.json({
+      success: true,
+      username: users.username + ' successfully register please Signin',
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send('something failed');
+  }
+});
 
-  module.exports=router;
+router.get('/chat/:id', auth, async (req, res) => {
+  try {
+    console.log('hello');
+    const user = await Users.findOne({ _id: req.params.id });
+    return res.json(user.chats);
+  } catch (errror) {
+    res.status(500).send('something failed');
+  }
+});
 
- 
+router.get('/users', auth, (req, res) => {
+  Users.find()
+    .then((obj) => {
+      sendsUserData(obj);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+sendsUserData = (obj) => {
+  client.emit('userdata', obj);
+};
+
+module.exports = router;
