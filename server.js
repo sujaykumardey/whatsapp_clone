07269ce68f3e2f355d685eb9c69fc8eb397f87c2
sujay dey <sujay.dey@mountblue.tech>
2020-06-19 01:44:13 +0530

    dsfsd

--- server.js
+++ server.js
@@ -10,7 +10,18 @@ const bodyParser = require('body-parser');
 const { url } = require('./config/key');
 
 app.use('/upload', express.static('upload'));
-app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+  );
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
